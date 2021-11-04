c33ba3f8d185f4c06adba62bd18bae674fbebb82
Rennan Cavalcante <rennanrene@gmail.com>
2021-11-04 13:01:26 -0300

    Resolucao Ex 05

---
+++ [M1S03] - Ex 5 - Lâmpada/script.js
@@ -0,0 +1,21 @@
+var estado = false;
+
+var elemento = document.createElement("img");
+document.getElementById("container").appendChild(elemento);
+imagemAtual(estado);
+
+function ligaDesliga(){
+    estado = !estado;
+    imagemAtual(estado);
+}
+
+function imagemAtual(estado){
+
+    if (estado == true){
+       elemento.setAttribute("src","imagens/luz-acesa.png");
+    }
+    if (estado == false){
+       elemento.setAttribute("src","imagens/luz-apagada.png");
+    }
+
+}
