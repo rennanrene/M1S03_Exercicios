7e536c39f04bf833fb63c3ece37dfc291137aa84
Rennan Cavalcante <rennanrene@gmail.com>
2021-11-04 14:22:54 -0300

    Resolucao Ex 07

---
+++ [M1S03] - Ex 7 - Salvando status da lâmpada/script.js
@@ -0,0 +1,25 @@
+var estadoRecuperado = localStorage.getItem("status");
+var estado = estadoRecuperado === "true";
+
+
+
+var elemento = document.createElement("img");
+document.getElementById("container").appendChild(elemento);
+imagemAtual(estado);
+
+function ligaDesliga(){
+    estado = !estado;
+    imagemAtual(estado);
+    localStorage.setItem("status", estado);
+}
+
+function imagemAtual(estado){
+
+    if (estado == true){
+       elemento.setAttribute("src","imagens/luz-acesa.png");
+    }
+    if (estado == false){
+       elemento.setAttribute("src","imagens/luz-apagada.png");
+    }
+
+}
